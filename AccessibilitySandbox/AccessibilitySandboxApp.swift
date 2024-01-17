499921bfcb96194190f8c7dcb55ed41cbd1eafde
Scott Crowder <scrowder@gmail.com>
2024-01-17 15:13:20 -0600

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by SCOTT CROWDER on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
