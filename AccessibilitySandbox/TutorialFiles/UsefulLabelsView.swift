ff567d13c194025a4c18291785a89b0ff0185358
Scott Crowder <scrowder@gmail.com>
2024-01-19 11:06:46 -0600

    Add screen reader labels

---
+++ AccessibilitySandbox/TutorialFiles/UsefulLabelsView.swift
@@ -0,0 +1,43 @@
+//
+//  UsefulLabelsView.swift
+//  AccessibilitySandbox
+//
+//  Created by SCOTT CROWDER on 1/17/24.
+//
+
+import SwiftUI
+
+struct UsefulLabelsView: View {
+    
+    let pictures: [String] = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    
+    let labels: [String] = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks"
+    ]
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+    
+    var body: some View {
+        Image(pictures[selectedPicture])
+            .resizable()
+            .scaledToFit()
+            .onTapGesture {
+                selectedPicture = Int.random(in: 0...3)
+            }
+            .accessibilityLabel(labels[selectedPicture])
+            .accessibilityAddTraits(.isButton)
+            .accessibilityRemoveTraits(.isImage)
+    }
+}
+
+#Preview {
+    UsefulLabelsView()
+}
